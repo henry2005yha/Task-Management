041bb2baa6b86a93b430c1dda7a3f8902c04ed47
lenovo <jj446708@gmail.com>
2024-05-24 14:42:51 +0630

    Your commit message here

---
+++ src/Components/Authentication Components/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.get('localhost:8080/taskmanagement/user/test');
+            console.log(response.data);
+        } catch (error) {
+            console.error('Login Error! Try Again', error);
+        }
+    };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" required/>
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" required/>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
