7072661c9a1520059aeadd99102617e3a84c66c6
lenovo <jj446708@gmail.com>
2024-05-27 15:39:49 +0630

    New

---
+++ src/Components/Task Managment Components/AddCategory.jsx
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const AddCategory = () => {
+    const [addCategory, setAddCategory] = useState('');
+    const [catDescription, setCatDescription] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:8080/taskmanagement/task/addCategory', {addCategory, catDescription});
+            const addCatData = response.data;
+            console.log(addCatData);
+        } catch (error) {
+            console.error('Add Category Error, Try Again!', error);
+        }
+    }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={addCategory} onChange={(e) => setAddCategory(e.target.value)} placeholder='Enter Category' required />
+        <textarea value={catDescription} onChange={(e) => setCatDescription(e.target.value)} placeholder='Enter Description' required></textarea>
+      </form>
+    </div>
+  )
+}
+
+export default AddCategory
