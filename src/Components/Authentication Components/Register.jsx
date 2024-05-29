3cddc3c0ec7cf99debc1b03d6de948c200a39755
lenovo <jj446708@gmail.com>
2024-05-29 09:43:11 +0630

    new

--- src/Components/Authentication Components/Register.jsx
+++ src/Components/Authentication Components/Register.jsx
@@ -4,14 +4,14 @@ import './register.css'
 import axiosInstance from '../AxiosHelper';
 const Register = () => {
     
-    const [userName, setUsername] = useState('');
+    const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          const response = await axiosInstance.post('/user/register', { userName, email, password });
+          const response = await axiosInstance.post('/user/register', { username, email, password });
           localStorage.setItem('email', response.data.email);
           window.location.href = '/dashboard';
         } catch (error) {
@@ -22,7 +22,7 @@ const Register = () => {
     <div className='register-container'>
       <form onSubmit={handleSubmit} className='register-form'>
       <h2>Register</h2>
-        <input type="text" value={userName} onChange={(e) =>setUsername(e.target.value)} placeholder='Enter Username' required className="register-input"/>
+        <input type="text" value={username} onChange={(e) =>setUsername(e.target.value)} placeholder='Enter Username' required className="register-input"/>
         <input type="email" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder='Enter Email' required className="register-input"/>
         <input type="password" value={password} onChange={(e) =>setPassword(e.target.value)} placeholder='Enter Password' required className="register-input"/>
         <button type='submit' className="register-button">Register</button>
