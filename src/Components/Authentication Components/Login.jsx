0f23e9210bb15f064e28d2fdf63b250cbc15564e
lenovo <jj446708@gmail.com>
2024-06-10 15:30:50 +0630

    updated version

--- src/Components/Authentication Components/Login.jsx
+++ src/Components/Authentication Components/Login.jsx
@@ -11,23 +11,20 @@ const Login = () => {
     e.preventDefault();
     try {
       const response = await axiosInstance.post('/user/login', { email, password });
-
-      if (response.data.success) { // Assuming your backend sends a success field on successful login
-        localStorage.setItem('email', response.data.email);
-        console.log('Login successful');
-        swal("Success!", "Login successful", "success")
-          .then(() => {
-            window.location.href = '/dashboard'; // Redirect to dashboard
-          });
-      } else {
-        swal("Error!", "Login failed. Please check your email and password.", "error");
-      }
+      localStorage.setItem('email', response.data.email);
+      console.log('Login successful');
+      swal("Success!", "Login successful", "success")
+        .then(() => {
+          window.location.href = '/dashboard'; // Redirect to dashboard
+        });
     } catch (error) {
       console.error('Login Error! Try Again', error);
+      // Show error message
       swal("Error!", "Login failed. Please check your email and password.", "error");
     }
   };
 
+  // Function to handle redirect to registration page
   const handleRegisterClick = () => {
     window.location.href = '/register'; // Redirect to registration page
   };
@@ -38,11 +35,11 @@ const Login = () => {
         <h2>Login</h2>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter Email" required className='login-input' />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter Password" required className='login-input' />
-        <p onClick={handleRegisterClick} className='register-link'>Register Now</p>
+        <p onClick={handleRegisterClick} className='register-link'>Register Now</p> {/* Add onClick handler */}
         <button type='submit' className='login-button'>Login</button>
       </form>
     </div>
-  );
-};
+  )
+}
 
 export default Login;
